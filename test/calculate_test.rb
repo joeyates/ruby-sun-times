fd998d42fb16a52f18ff3d16287976e6556aa0d3
Joe Yates <joe.g.yates@gmail.com>
2011-12-19 23:59:50 +0000

    Added test with other time formats: Time and DateTime

--- test/calculate_test.rb
+++ test/calculate_test.rb
@@ -37,4 +37,16 @@ class SunTimesTest < Test::Unit::TestCase
     assert_raise(RuntimeError) { SunTimes.calculate(:foo, Date.new(2010, 3, 8), 43.779, 11.432) }
   end
 
+  def test_time
+    datetime = Time.gm(2010, 6, 13, 0, 0, 0)
+    set = SunTimes.calculate(:set, datetime, 43.779, 11.432)
+    assert_equal(Time.gm(2010, 6, 13, 18, 56, 55), set)
+  end
+
+  def test_datetime
+    datetime = DateTime.new(2010, 6, 13, 0, 0, 0)
+    set = SunTimes.calculate(:set, datetime, 43.779, 11.432)
+    assert_equal(Time.gm(2010, 6, 13, 18, 56, 55), set)
+  end
+
 end
