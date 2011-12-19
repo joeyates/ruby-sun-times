3712bc90ed06cd4f0568656a8e4e5ab5d26bc3f0
Joe Yates <joe.g.yates@gmail.com>
2011-12-19 23:58:25 +0000

    Use time formats in test comparisons

--- test/calculate_test.rb
+++ test/calculate_test.rb
@@ -2,27 +2,28 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 require 'test/unit'
 require 'sun_times'
+require 'date'
 
 class SunTimesTest < Test::Unit::TestCase
 
   def test_rise_20100308_pontassieve
     rise = SunTimes.calculate(:rise, Date.new(2010, 3, 8), 43.779, 11.432)
-    assert_equal(rise.to_i, 1268026793)
+    assert_equal(Time.gm(2010, 3, 8, 5, 39, 53), rise)
   end
 
   def test_set_20100308_pontassieve
-    rise = SunTimes.calculate(:set, Date.new(2010, 3, 8), 43.779, 11.432)
-    assert_equal(rise.to_i, 1268068276)
+    set = SunTimes.calculate(:set, Date.new(2010, 3, 8), 43.779, 11.432)
+    assert_equal(Time.gm(2010, 3, 8, 17, 11, 16), set)
   end
 
   def test_rise_helper
     rise = SunTimes.rise(Date.new(2010, 3, 8), 43.779, 11.432)
-    assert_equal(rise.to_i, 1268026793)
+    assert_equal(Time.gm(2010, 3, 8, 5, 39, 53), rise)
   end
 
   def test_set_helper
-    rise = SunTimes.set(Date.new(2010, 3, 8), 43.779, 11.432)
-    assert_equal(rise.to_i, 1268068276)
+    set = SunTimes.set(Date.new(2010, 3, 8), 43.779, 11.432)
+    assert_equal(Time.gm(2010, 3, 8, 17, 11, 16), set)
   end
 
   def test_midnight_sun_on_20100621_north_cape
